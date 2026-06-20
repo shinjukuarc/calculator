4311719476e3b578bf3443c48037153727c258d2
shinjukuarc <omar.elafry@gmail.com>
2026-06-21 00:53:27 +0300

    version 3:
    --added the result/equal button functionality
    >now the user can see the result of calculations
    --fixed the infinity showing up error due to js
    >so no infinity show up as a result
    --NaN and undefined can't be used in calculations
    >so no text is used in calculations

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ function divide(a,b) {
 }
 
 function operate(num1, operator, num2) {
-    if (operator==='+') {add(num1,num2)}
-    if (operator==='-') {subtract(num1,num2)}
-    if (operator==='*') {multiply(num1,num2)}
-    if (operator==='/') {divide(num1,num2)}
+    if (operator==='+') {return add(num1,num2)}
+    if (operator==='−') {return subtract(num1,num2)}
+    if (operator==='×') {return multiply(num1,num2)}
+    if (operator==='÷') {return divide(num1,num2)}
 }
 
 let num1='', operator='', num2=''
@@ -35,18 +35,23 @@ const calculator = document.querySelector('.calc')
 const screen = document.querySelector('.screen')
 
 let stage=1
+let result
 
 numbers.forEach(element=> {
     element.addEventListener('click', ()=> {
-        if (stage===1 || stage===3) {
+        if ((stage===1 || stage===3) && (num1!=='undefined' && num1!=='NaN')) {
             appendToCalc(element.textContent, screen);
             (stage===1) ? num1+=element.textContent : num2+=element.textContent
+        } else if (num1==='undefined' || num1==='NaN') {
+            screen.textContent=''
+            appendToCalc(element.textContent, screen);
+            (stage===1) ? num1=element.textContent : num2=element.textContent
         }
     })
 })
 operators.forEach(element=> {
     element.addEventListener('click', ()=> {
-        if (stage===1) {
+        if ((stage===1 && num1!=='') && (num1!=='undefined' && num1!=='NaN')) {
             appendToCalc(element.textContent, screen)
             stage=3
             operator=element.textContent
@@ -59,8 +64,24 @@ backSpace.addEventListener('click', (e)=> {
     } else if (stage===3 && num2==='') {
         operator=''
         stage=1
-    } else if (stage===1) {
+    } else if (stage===1 && (num1!=='undefined' && num1!=='NaN' && num1!=='Infinity')) {
         num1=num1.slice(0,-1)
+    } else if (num1==='undefined' || num1==='NaN') {
+        num1=''
     }
     screen.textContent=num1+operator+num2
+})
+equal.addEventListener('click', ()=> {
+    if (num1!=='' && operator!=='' && num2!=='') {
+        result=operate(+num1,operator,+num2)
+        num1=`${result}`
+        if (operator==='÷' && num2==='0') {
+            result='undefined'
+            num1=`${result}`
+        }
+        screen.textContent=result
+        result=''
+        stage=1
+        operator='', num2=''
+    }
 })
